f5b7ed40fd53bc0c4ce8b327f815ddb81c03cc5a
Saimun Hassan <saimun4u@gmail.com>
2023-06-03 14:34:33 -0400

    Logic completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 email = input('Enter your email: ')
-k, j=0,0
+k,j,d=0,0,0
 if len(email) >= 6:
     if email[0].isalpha():
         if ('@' in email) and (email.count("@") == 1): 
@@ -12,7 +12,11 @@ if len(email) >= 6:
                           j=1
                   elif i.isdigit():
                       continue
-              if k==1 or j==1:
+                  elif i=="_" or i=="." or i=="@":
+                      continue
+                  else:
+                      d=1
+              if k==1 or j==1 or d==1:
                   print('The email is incorrect')                      
           else:
               print('The email address entered is not valid')
