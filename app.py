6481a1f6415b2a68a4f050f5bcb006b8f53937e6
Saimun Hassan <saimun4u@gmail.com>
2023-06-03 14:55:37 -0400

    Debugged the code for bugs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ k,j,d=0,0,0
 if len(email) >= 6:
     if email[0].isalpha():
         if ('@' in email) and (email.count("@") == 1): 
-          if (email[-4]==".") ^ email[-3]==".":
+          if (email[-4]==".") ^ (email[-3]=="."):
               for i in email:
                   if i ==i.isspace():
                       k=1
@@ -17,9 +17,10 @@ if len(email) >= 6:
                   else:
                       d=1
               if k==1 or j==1 or d==1:
-                  print('The email is incorrect')                      
-          else:
-              print('The email address entered is not valid')
-    print('The email address entered is not valid')
+                  print('The email is incorrect')  
+              else:
+                  print('Nice! This is a valid email address')                    
+        else:
+          print('The email address entered is not valid')
 else:
     print('The email address is not valid')
