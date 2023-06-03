547e78d7e9a8e9a752378d061bfff2251ac57ec3
Saimun Hassan <saimun4u@gmail.com>
2023-06-03 14:10:51 -0400

    Check conditions for alphabet and uppercase

---
+++ app.py
@@ -0,0 +1,18 @@
+email = input('Enter your email: ')
+k = 0
+if len(email) >= 6:
+    if email[0].isalpha():
+        if ('@' in email) and (email.count("@") == 1): 
+          if (email[-4]==".") ^ email[-3]==".":
+              for i in email:
+                  if i ==i.isspace():
+                      k=1
+                  elif i.isalpha():
+                      if i==i.upper()
+              if k==1:
+                  print('The email is incorrect')                      
+          else:
+              print('The email address entered is not valid')
+    print('The email address entered is not valid')
+else:
+    print('The email address is not valid')
